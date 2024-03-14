e47136dc0873e0a7bc4516f733e11d76fe504833
wisdomwest <harmonykoome@gmail.com>
2024-03-15 00:15:31 +0300

    pycodestyle issue

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1 
+Each box is numbered sequentially from 0 to n - 1
 and each box may contain keys to the other boxes.
 
 Write a method that determines if all the boxes can be opened.
@@ -13,6 +13,8 @@ There can be keys that do not have boxes
 The first box boxes[0] is unlocked
 Return True if all boxes can be opened, else return False
 '''
+
+
 def canUnlockAll(boxes):
     if not boxes or type(boxes) is not list:
         return False
