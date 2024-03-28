8813dbf6ad550bf2c4f3caf4e6b347f350f1e6de
wisdomwest <harmonykoome@gmail.com>
2024-03-28 22:00:57 +0300

    lets see

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,5 +13,32 @@ if __name__ == "__main__":
                 print("{}: {}".format(key, value))
 
     file_size = 0
+    found = {}
     status = {"200", "301", "400", "401", "403", "404", "405", "500"}
+    count = 0
 
+    try:
+        for line in sys.stdin:
+            try:
+                word = line[:-1].split()
+
+                size = int(word[-1])
+                file_size += size
+
+                code = int(word[-2])
+                if str(code) in status:
+                    found[str(code)] = found.get(str(code), 0) + 1
+
+            except BaseException:
+                pass
+
+            count += 1
+
+            if count % 10 == 0:
+                print_stats(file_size, found)
+
+    except KeyboardInterrupt:
+        print_stats(file_size, found)
+        raise
+
+    print_stats(file_size, found)
