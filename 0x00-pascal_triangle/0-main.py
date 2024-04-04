88a714389d1807aee7163354997bc0f871091b8e
wisdomwest <harmonykoome@gmail.com>
2024-04-04 14:02:22 +0300

    change

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -14,3 +14,6 @@ def print_triangle(triangle):
 
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
+    print_triangle(pascal_triangle(0))
+    print_triangle(pascal_triangle(1))
+    print_triangle(pascal_triangle(10))
