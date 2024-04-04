2041f524ef0f13d71da1639a7fb88427ef266a8a
wisdomwest <harmonykoome@gmail.com>
2024-04-04 13:58:47 +0300

    got a checker wrong

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,27 @@
 def validUTF8(data):
     '''validate utf-8 encoding'''
     '''
-    longer code ->
+    n_bytes = 0
+
+    for byte in data:
+        if n_bytes == 0:
+            if (byte >> 7) == 0b0:
+                continue
+            elif (byte >> 5) == 0b110:
+                n_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                n_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                n_bytes = 3
+            else:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            n_bytes -= 1
+
+    return n_bytes == 0
+    '''
 
     n_bytes = 0
 
@@ -33,24 +53,3 @@ def validUTF8(data):
         return True
 
     return False
-    '''
-    n_bytes = 0
-
-    for byte in data:
-        if n_bytes == 0:
-            if (byte >> 7) == 0b0:
-                continue
-            elif (byte >> 5) == 0b110:
-                n_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                n_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                n_bytes = 3
-            else:
-                return False
-        else:
-            if (byte >> 6) != 0b10:
-                return False
-            n_bytes -= 1
-
-    return n_bytes == 0
