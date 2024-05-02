4036b423550a2646857e586e7c1806720b9117d0
wisdomwest <harmonykoome@gmail.com>
2024-05-02 16:47:44 +0300

    make change problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''refer to readme for more information'''
+
+def makeChange(coins, total):
+    '''makeChange function'''
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        count += total // coin
+        total = total % coin
+    if total != 0:
+        return -1
+    return count
