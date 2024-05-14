b247288ddf8751453df3321edda790fa0585afe8
wisdomwest <harmonykoome@gmail.com>
2024-05-14 13:57:10 +0300

    pycodestyle issues

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''refer to the README.md file for the problem statement'''
 
+
 def is_prime(num):
     if num < 2:
         return False
@@ -9,19 +10,20 @@ def is_prime(num):
             return False
     return True
 
+
 def isWinner(x, nums):
     maria_wins = 0
     ben_wins = 0
-    
+
     for i in range(x):
         n = nums[i]
         primes = [i for i in range(1, n + 1) if is_prime(i)]
-        
+
         if len(primes) % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
